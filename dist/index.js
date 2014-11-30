6ac2bafaa4918a674d0921eb3361b995da39823d
Elie Rotenberg <elie@rotenberg.io>
2014-11-30 20:28:41 +0100

    Upgraded deps.

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require("6to5/polyfill");var Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;var _ = require("lodash-next");
+require("6to5/polyfill");var Promise = (global || window).Promise = require("lodash-next").Promise;var __DEV__ = (process.env.NODE_ENV !== "production");var __PROD__ = !__DEV__;var __BROWSER__ = (typeof window === "object");var __NODE__ = !__BROWSER__;var _ = require("lodash-next");
 var recase = require("change-case");
 
 function extractStyle(selector, reactStyle) {
