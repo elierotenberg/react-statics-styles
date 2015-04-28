5160a8377022c170c33599bc6c47239020aaf791
Clément Fiorio <clement.fiorio@gmail.com>
2015-04-28 17:08:39 +0200

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,56 +12,31 @@ if(__DEV__) {
 
 var del = require('del');
 var babel = require('gulp-babel');
-<<<<<<< HEAD
-var fs = Promise.promisifyAll(require('fs'));
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var jshint = require('gulp-jshint');
-=======
 var eslint = require('gulp-eslint');
 var fs = Promise.promisifyAll(require('fs'));
 var gulp = require('gulp');
 var gutil = require('gulp-util');
->>>>>>> starterkit/master
 var plumber = require('gulp-plumber');
 var prepend = require('gulp-insert').prepend;
-var sourcemaps = require('gulp-sourcemaps');
-var stylish = require('jshint-stylish');
-
-var readPrelude = fs.readFileAsync('./__prelude.js');
 
 var readPrelude = fs.readFileAsync('./__prelude.js');
 
 function lint() {
   return gulp.src('src/**/*.js')
   .pipe(plumber())
-<<<<<<< HEAD
-  .pipe(jshint())
-  .pipe(jshint.reporter(stylish));
-=======
   .pipe(eslint())
   .pipe(eslint.format());
->>>>>>> starterkit/master
 }
 
 function build() {
   return readPrelude.then(function(prelude) {
     return gulp.src('src/**/*.js')
-<<<<<<< HEAD
-      .pipe(plumber())
-      .pipe(prepend(prelude))
-      .pipe(babel({
-        modules: 'common',
-      }))
-      .pipe(gulp.dest('dist'));
-=======
     .pipe(plumber())
     .pipe(prepend(prelude))
     .pipe(babel({
       modules: 'common',
     }))
     .pipe(gulp.dest('dist'));
->>>>>>> starterkit/master
   });
 }
 
